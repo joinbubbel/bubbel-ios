abba86e8ea84b4f5fa7e0472f784d8f852abff26
Ivoine <ivoinestrachan242@gmail.com>
2023-06-30 18:20:39 -0400

    intial commit

---
+++ bubbel/bubbelApp.swift
@@ -0,0 +1,17 @@
+//
+//  bubbelApp.swift
+//  bubbel
+//
+//  Created by Ivoine Strachan on 30/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct bubbelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
