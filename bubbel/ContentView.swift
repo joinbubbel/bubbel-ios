44fd2fdcbdb655b054cf1e028c60e98a39fa6083
Ivoine <ivoinestrachan242@gmail.com>
2023-06-30 21:08:50 -0400

    worked on login / setting everything up

--- bubbel/ContentView.swift
+++ bubbel/ContentView.swift
@@ -7,20 +7,66 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	@State private var username: String = ""
+	@State private var password: String = ""
+	@FocusState private var keyboardFocused: Bool
+	var body: some View {
+		VStack {
+			
+			Text("")
+			.padding(110)
+			.background(
+			Image("LoginBanner")
+				.resizable()
+				.aspectRatio(contentMode: .fit)
+				.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+			)
+			.padding(10)
+			Text("Username")
+				.padding(.trailing, 290)
+			TextField("jonedoe", text: $username)
+				.padding(.leading, 15)
+			Rectangle()
+			.frame(height: 1.0, alignment: .bottom)
+				.foregroundColor(Color.gray)
+				.baselineOffset(10)
+				.focused($keyboardFocused)
+				.font(.system(size: 16))
+				.padding(.trailing, 10)
+				.padding(.leading, 10)
+				.onAppear {
+					DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+						keyboardFocused = true
+					}
+				}
+			
+			Text("Password")
+				.padding(.trailing, 290)
+			TextField("••••••••", text: $password)
+				.padding(.leading, 15)
+			Rectangle()
+			.frame(height: 1.0, alignment: .bottom)
+				.foregroundColor(Color.gray)
+				.baselineOffset(10)
+				.focused($keyboardFocused)
+				.font(.system(size: 16))
+				.padding(.trailing, 10)
+				.padding(.leading, 10)
+				.onAppear {
+					DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+						keyboardFocused = true
+					}
+				}
+			Spacer()
+		}
+		.edgesIgnoringSafeArea(.all)
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
