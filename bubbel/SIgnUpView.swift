237363fab7879d106da12a2a3851e2a971b6cf75
Ivoine <ivoinestrachan242@gmail.com>
2023-07-06 18:18:00 -0400

    crashed database

--- bubbel/SIgnUpView.swift
+++ bubbel/SIgnUpView.swift
@@ -7,6 +7,73 @@
 
 import SwiftUI
 
+class InCreateUser: Codable {
+    var email: String
+    var username: String
+    var password: String
+    
+    init(email: String, username: String, password: String) {
+        self.email = email
+        self.username = username
+        self.password = password
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        email = try container.decode(String.self, forKey: .email)
+        username = try container.decode(String.self, forKey: .username)
+        password = try container.decode(String.self, forKey: .password)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(email, forKey: .email)
+        try container.encode(username, forKey: .username)
+        try container.encode(password, forKey: .password)
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case email
+        case username
+        case password
+    }
+}
+
+class DatabaseError: Codable {
+    var type: String
+    var uerror: String?
+}
+
+class CreateUserError: Codable {
+    var type: String
+    var dberror: DatabaseError?
+}
+
+class ResCreateUser: Codable {
+    var error: CreateUserError?
+}
+
+class InAuthUser: Codable {
+    var username: String
+    var password: String
+}
+
+class AuthUserError: Codable {
+    var type: String
+    var dberror: DatabaseError?
+}
+
+class ResAuthUser: Codable {
+    var error: AuthUserError?
+    var token: String
+    var username: String
+    var email: String
+}
+
+class InDeauthUser: Codable {
+    var token: String
+}
+
 struct SignUpView: View {
     @State private var username: String = ""
     @State private var password: String = ""
@@ -15,7 +82,7 @@ struct SignUpView: View {
     @FocusState private var keyboardFocused: Bool
     
     
-    func SignUp() {
+    func SignUp() async {
         guard isValidEmail(email) else {
             print("Invalid email format")
             return
@@ -26,7 +93,50 @@ struct SignUpView: View {
             return
         }
         
-        print("Sign Up successful")
+        let createUserRequest = InCreateUser(email: email, username: username, password: password)
+        do {
+            let response = try await createUserAPIRequest(request: createUserRequest)
+            // Handle the response
+            if response.error != nil {
+                // Handle CreateUserError
+            } else {
+                // User creation success
+                _ = response.token
+                _ = response.username
+                _ = response.email
+                
+            }
+        } catch {
+            // Handle the error
+            print("Error: \(error)")
+        }
+    }
+    
+    
+    func createUserAPIRequest(request: InCreateUser) async throws -> ResAuthUser {
+        let encoder = JSONEncoder()
+        let json = try encoder.encode(request)
+        let jsonString = String(data: json, encoding: .utf8) ?? ""
+            print(jsonString)
+        let url = URL(string: bubbelBathDev + "/create_user")! // Update the endpoint based on your API
+        var urlRequest = URLRequest(url: url)
+        urlRequest.addValue("Content-Type", forHTTPHeaderField: "application/json")
+        urlRequest.httpMethod = "POST"
+        urlRequest.httpBody = json
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        let (dataString) = String(data: data, encoding: .utf8) ?? ""
+        print(dataString)
+        
+        /*
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw BubbelError.fetchError
+        }
+         */
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let result = try decoder.decode(ResAuthUser.self, from: data)
+        return result
     }
     
     private func isValidEmail(_ email: String) -> Bool {
@@ -40,6 +150,62 @@ struct SignUpView: View {
         let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
         return passwordPredicate.evaluate(with: password)
     }
+    
+    let bubbelBathDev = "https://bubbel-bath.onrender.com/api";
+    
+    enum BubbelError: Error {
+        case fetchError
+    }
+    
+    func bubbelApiAuthUser(bath: String, req: InAuthUser) async throws -> ResAuthUser {
+        let encoder = JSONEncoder()
+        let json = try encoder.encode(req)
+        let url = URL(string: bath + "/api/auth_user")!
+        var request = URLRequest(url: url)
+        request.addValue("Content-Type", forHTTPHeaderField: "application/json")
+        request.httpMethod = "POST"
+        request.httpBody = json
+        let (data, response) = try await URLSession.shared.data(for: request)
+        /*
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            throw BubbelError.fetchError
+        }
+         */
+        if let dataString = String(data: data, encoding: .utf8) {
+
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            guard let jsonData = dataString.data(using: .utf8) else {
+                throw BubbelError.fetchError
+            }
+            print(dataString)
+            return try decoder.decode(ResAuthUser.self, from: jsonData)
+        } else {
+            throw BubbelError.fetchError
+        }
+    }
+    
+    
+    
+    
+    
+    func bubbelApiDeauthUser(bath: String, req: InDeauthUser) async throws {
+        let encoder = JSONEncoder()
+        let json = try encoder.encode(req)
+        let url = URL(string: bath + "/api/deauth_user")!
+        var request = URLRequest(url: url)
+        request.addValue("Content-Type", forHTTPHeaderField: "application/json")
+        request.httpMethod = "POST"
+        request.httpBody = json
+        let (_, response) = try await URLSession.shared.data(for: request)
+/*        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            throw BubbelError.fetchError
+        }
+ */
+    }
+    
     var body: some View {
         VStack{
             Text("")
@@ -68,6 +234,7 @@ struct SignUpView: View {
             HStack{
                 Image("picon")
                 TextField("John Doe", text: $username)
+                    .autocapitalization(.none)
                     .font(Font.custom("CircularStd-Book", size: 16))
                     .foregroundColor(.black)
             }
@@ -148,7 +315,11 @@ struct SignUpView: View {
                 }
             
             VStack{
-                Button(action: SignUp){
+                Button(action: {
+                    Task {
+                        await SignUp()
+                    }
+                }) {
                     Text("Sign Up")
                         .font(Font.custom("CircularStd-Book", size: 16))
                         .multilineTextAlignment(.center)
@@ -169,6 +340,10 @@ struct SignUpView: View {
             .navigationBarBackButtonHidden(true)
             .edgesIgnoringSafeArea(.all)
         }
+        .task {
+            await SignUp()
+        }
+        
     }
     
     
