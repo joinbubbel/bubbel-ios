5accac6028838116e19bbeb404deb263e69c5451
dav <usr21p1@gmail.com>
2023-07-21 15:00:32 -0700

    Update Backend.swift

--- bubbel/Backend.swift
+++ bubbel/Backend.swift
@@ -739,12 +739,6 @@ extension ResDeauthUser {
 // MARK: - InVerifyAccount
 struct InVerifyAccount: Codable {
     let code: String
-    let userID: Int
-
-    enum CodingKeys: String, CodingKey {
-        case code
-        case userID = "user_id"
-    }
 }
 
 // MARK: InVerifyAccount convenience initializers and mutators
@@ -766,12 +760,10 @@ extension InVerifyAccount {
     }
 
     func with(
-        code: String? = nil,
-        userID: Int? = nil
+        code: String? = nil
     ) -> InVerifyAccount {
         return InVerifyAccount(
-            code: code ?? self.code,
-            userID: userID ?? self.userID
+            code: code ?? self.code
         )
     }
 
@@ -868,8 +860,7 @@ extension VerifyAccountError {
 }
 
 enum StickyType: String, Codable {
-    case codeTimedOutOrInvalidUser = "CodeTimedOutOrInvalidUser"
-    case invalidCode = "InvalidCode"
+    case codeTimedOutOrAlreadyVerifiedOrInvalidCode = "CodeTimedOutOrAlreadyVerifiedOrInvalidCode"
     case typeInternal = "Internal"
 }
 
@@ -1171,7 +1162,6 @@ func bubbelApiDeleteUser(req: InDeleteUser) async throws -> ResDeleteUser {
             let result = try decoder.decode(ResDeleteUser.self, from: data)
             return result
         }
-
 
 struct Backend: View {
     var body: some View {
