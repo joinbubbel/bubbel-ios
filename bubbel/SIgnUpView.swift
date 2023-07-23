b1aba0068f35001d00cfac402d0b6cb18003c7c4
Ivoine <ivoinestrachan242@gmail.com>
2023-07-23 17:56:27 -0400

    error validation works for signup but docent show error in the textfield

--- bubbel/SIgnUpView.swift
+++ bubbel/SIgnUpView.swift
@@ -11,35 +11,78 @@ struct SignUpView: View {
     @State private var showVerificationView = false
     @State private var verificationResponse: ResSendVerify?
     
+    @State private var errorMessage: String = ""
     
     @State private var userID: Int?
+    
+    
+    
+    private var usernamePlaceholder: String {
+        return errorMessage.isEmpty ? "John Doe" : errorMessage
+    }
+    
+    private var emailPlaceholder: String {
+        return errorMessage.isEmpty ? "johndoe@gmail.com" : errorMessage
+    }
+    
+    private var passwordPlaceholder: String {
+        return errorMessage.isEmpty ? "••••••••" : errorMessage
+    }
+    var isSignUpButtonDisabled: Bool {
+        return username.isEmpty || email.isEmpty || password.isEmpty
+    }
+    
+    func isUsernameAvailable() async -> Bool {
+        do {
+            let createUserRequest = InCreateUser(email: email, password: password, username: username)
+            let createUserResponse = try await bubbelApiCreateUser(req: createUserRequest)
+            return createUserResponse.error?.type != .emailOrUsernametaken
+        } catch {
+            return true
+        }
+    }
+    
     
     func createUser() async {
         do {
             let createUserRequest = InCreateUser(email: email, password: password, username: username)
             let createUserResponse = try await bubbelApiCreateUser(req: createUserRequest)
-            print("heres repsonse",createUserResponse)
+            print("heres response", createUserResponse)
             if let error = createUserResponse.error {
+                switch error.type {
+                case .emailOrUsernametaken:
+                    errorMessage = "Email or Username is already taken."
+                case .invalidEmail:
+                    errorMessage = "Invalid email format."
+                case .invalidPassword:
+                    errorMessage = "Invalid password format."
+                case .invalidPasswordCryto:
+                    errorMessage = "Password encryption failed."
+                case .invalidUsername:
+                    // No error message for invalid username, do nothing here.
+                    break
+                case .typeInternal:
+                    errorMessage = "Internal server error."
+                case .sendVerification:
+                    errorMessage = "Failed to send verification email."
+                }
                 return
             } else {
                 // User created successfully, assign the user ID
-                // userID = createUserResponse.userID
                 if let userID = createUserResponse.userID {
-                    print("Text Before",userID)
+                    print("Text Before", userID)
                     let sendVerifyEmailRequest = InSendVerify(userID: userID)
                     let sendVerifyEmailResponse = try await bubbelApiSendVerify(req: sendVerifyEmailRequest)
+                    // Perform any necessary actions after successful signup and verification
                 } else {
-                    print("Everything fUCKED UP")
-                    // Handle the case where userID is nil (optional was not set in the response)
-                    // Do something appropriate for your use case
+                    print("Everything is messed up")
                 }
             }
         } catch {
+            errorMessage = "An unexpected error occurred."
             print("Error: \(error)")
         }
     }
-    
-    
     
     
     
@@ -71,18 +114,23 @@ struct SignUpView: View {
                 .padding(.top, -200)
             HStack{
                 Image("picon")
-                TextField("John Doe", text: $username)
+                TextField(usernamePlaceholder, text: $username)
                     .disableAutocorrection(true)
                     .autocapitalization(.none)
                     .font(Font.custom("CircularStd-Book", size: 16))
-                    .foregroundColor(.black)
+                    .foregroundColor(keyboardFocused ? .black : (errorMessage.isEmpty ? .black : .red))
+                    .onTapGesture {
+                        if !errorMessage.isEmpty {
+                            errorMessage = ""
+                        }
+                    }
             }
             .padding(.top, -180)
             .padding(.leading, 20)
             
             Rectangle()
                 .frame(height: 1.0, alignment: .bottom)
-                .foregroundColor(Color.gray)
+                .foregroundColor(errorMessage.isEmpty ? Color.gray : Color.red)
                 .baselineOffset(10)
                 .focused($keyboardFocused)
                 .font(.system(size: 16))
@@ -103,17 +151,23 @@ struct SignUpView: View {
                     .padding(.top, -150)
                 HStack{
                     Image("mail")
-                    TextField("johndoe@gmail.com", text: $email)
+                    TextField(emailPlaceholder, text: $email)
                         .autocapitalization(.none)
                         .disableAutocorrection(true)
                         .font(Font.custom("CircularStd-Book", size: 16))
-                        .foregroundColor(.black)
+                        .foregroundColor(keyboardFocused ? .black : (errorMessage.isEmpty ? .black : .red))
+                        .onTapGesture {
+                            if !errorMessage.isEmpty {
+                                errorMessage = ""
+                            }
+                        }
+                    
                 }
                 .padding(.top, -110)
                 .padding(.leading, 20)
                 Rectangle()
                     .frame(height: 1.0, alignment: .bottom)
-                    .foregroundColor(Color.gray)
+                    .foregroundColor(errorMessage.isEmpty ? Color.gray : Color.red)
                     .baselineOffset(10)
                     .padding(.top, -80)
                     .padding(.trailing, 20)
@@ -123,6 +177,7 @@ struct SignUpView: View {
                         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                             keyboardFocused = true
                         }
+                        
                     }
             }
             
@@ -134,16 +189,22 @@ struct SignUpView: View {
                 .padding(.trailing, 280)
             HStack{
                 Image("lock")
-                TextField("••••••••", text: $password)
+                TextField(passwordPlaceholder, text: $password)
                     .autocapitalization(.none)
                     .font(Font.custom("CircularStd-Book", size: 16))
-                    .foregroundColor(.black)
+                    .foregroundColor(keyboardFocused ? .black : (errorMessage.isEmpty ? .black : .red))
+                    .onTapGesture {
+                        if !errorMessage.isEmpty {
+                            errorMessage = ""
+                        }
+                    }
+                
             }
             .padding(.top, -40)
             .padding(.leading, 20)
             Rectangle()
                 .frame(height: 1.0, alignment: .bottom)
-                .foregroundColor(Color.gray)
+                .foregroundColor(errorMessage.isEmpty ? Color.gray : Color.red)
                 .baselineOffset(10)
                 .padding(.trailing, 20)
                 .padding(.leading, 20)
@@ -158,8 +219,14 @@ struct SignUpView: View {
             VStack{
                 Button(action: {
                     Task {
-                        await createUser()
-                        showVerificationView = true
+                        if username.isEmpty || email.isEmpty || password.isEmpty {
+                            errorMessage = "Please fill in all the fields."
+                        } else if await isUsernameAvailable() {
+                            await createUser()
+                            showVerificationView = true
+                        } else {
+                            errorMessage = "Username or Email is already taken."
+                        }
                     }
                 }) {
                     Text("Sign Up")
