8f5a53fc3eab8f4e47120edd60dfe13d817bfaae
Ivoine <ivoinestrachan242@gmail.com>
2023-07-24 14:52:05 -0400

    refactoring some of tabview

--- bubbel/BottomView.swift
+++ bubbel/BottomView.swift
@@ -10,37 +10,46 @@ import SwiftUI
 struct BottomView: View {
     var username: String
     var body: some View {
-        TabView {
-            HomeView(username: username)
-                .tabItem {
-                    Image("Home")
-                        .renderingMode(.template)
-                        .foregroundColor(.blue)
-                }
-            Clubs()
-                .tabItem {
-                    Image("Clubs")
-                        .renderingMode(.template)
-                        .foregroundColor(.blue)
-                }
+        VStack{
             
-            Select()
-                .tabItem{
-                    Image("Add")
-                }
-            Message()
-                .badge(5)
-                .tabItem {
-                    Image("Message")
-                        .renderingMode(.template)
-                        .foregroundColor(.blue)
-                }
+            TabView {
+                HomeView(username: username)
+                    .tabItem {
+                        Image("Home")
+                            .renderingMode(.template)
+                            .foregroundColor(.blue)
+                    }
+                Clubs()
+                    .tabItem {
+                        Image("Clubs")
+                            .renderingMode(.template)
+                            .foregroundColor(.blue)
+                    }
+                
+                Select()
+                    .tabItem{
+                        Image("Add")
+                    }
+                Message()
+                    .badge(5)
+                    .tabItem {
+                        Image("Message")
+                            .renderingMode(.template)
+                            .foregroundColor(.blue)
+                    }
+                
+                Profile(username: username)
+                    .tabItem {
+                        Image("Profile")
+                        
+                    }
+            }
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(width: 400, height: 4)
+                .background(Color(red: 0.93, green: 0.94, blue: 0.95))
+                .padding(.top, -80)
             
-            Profile(username: username)
-                .tabItem {
-                    Image("Profile")
-                    
-                }
         }
     }
 }
