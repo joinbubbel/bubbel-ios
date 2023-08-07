e2d9f9f8ad3c35b00a6e48a2efdbbd3ac3d0faa4
Ivoine <ivoinestrachan242@gmail.com>
2023-08-07 16:03:27 -0400

    fixing navigation

--- bubbel/SIgnUpView.swift
+++ bubbel/SIgnUpView.swift
@@ -9,7 +9,7 @@ struct SignUpView: View {
     @State private var verificationCode: String = ""
     @State private var verificationEmailSent = false
     @State private var showVerificationView = false
-    @State private var verificationResponse: ResSendVerify?
+ 
     
     @State private var errorMessage: String = ""
     
@@ -79,72 +79,110 @@ struct SignUpView: View {
     
     
     var body: some View {
-        VStack{
-            Text("")
-                .padding(60)
-                .background(
-                    Image("SignupBanner")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-                )
+        NavigationView{
             VStack{
-                Text("Sign Up")
-                    .font(Font.custom("CircularStd-Medium", size: 26))
-                    .foregroundColor(.white)
-                    .position(x: 80, y: -90)
-                Text("Fill in the form to register")
-                    .font(Font.custom("CircularStd-Book", size: 18))
-                    .foregroundColor(.white)
-                    .position(x: 135, y: -180)
-            }
-            Text("Username")
-                .font(Font.custom("CircularStd-Book", size: 14))
-                .foregroundColor(Color(red: 0.39, green: 0.45, blue: 0.58))
-                .padding(.trailing, 270)
-                .padding(.top, -200)
-            HStack{
-                Image("picon")
-                TextField(usernamePlaceholder, text: $username)
-                    .disableAutocorrection(true)
-                    .autocapitalization(.none)
-                    .font(Font.custom("CircularStd-Book", size: 16))
-                    .foregroundColor(keyboardFocused ? .black : (errorMessage.isEmpty ? .black : .red))
-                    .onTapGesture {
-                        if !errorMessage.isEmpty {
-                            errorMessage = ""
-                        }
-                    }
-            }
-            .padding(.top, -180)
-            .padding(.leading, 20)
-            
-            Rectangle()
-                .frame(height: 1.0, alignment: .bottom)
-                .foregroundColor(errorMessage.isEmpty ? Color.gray : Color.red)
-                .baselineOffset(10)
-                .focused($keyboardFocused)
-                .font(.system(size: 16))
-                .padding(.trailing, 20)
-                .padding(.leading, 20)
-                .padding(.top, -155)
-                .onAppear {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                        keyboardFocused = true
-                    }
+                Text("")
+                    .padding(60)
+                    .background(
+                        Image("SignupBanner")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                    )
+                VStack{
+                    Text("Sign Up")
+                        .font(Font.custom("CircularStd-Medium", size: 26))
+                        .foregroundColor(.white)
+                        .position(x: 80, y: -90)
+                    Text("Fill in the form to register")
+                        .font(Font.custom("CircularStd-Book", size: 18))
+                        .foregroundColor(.white)
+                        .position(x: 135, y: -180)
                 }
-            VStack{
-                Text("Email Address")
+                Text("Username")
                     .font(Font.custom("CircularStd-Book", size: 14))
                     .foregroundColor(Color(red: 0.39, green: 0.45, blue: 0.58))
-                    .padding(.top, 20)
-                    .padding(.trailing, 250)
-                    .padding(.top, -150)
+                    .padding(.trailing, 270)
+                    .padding(.top, -200)
                 HStack{
-                    Image("mail")
-                    TextField(emailPlaceholder, text: $email)
-                        .autocapitalization(.none)
+                    Image("picon")
+                    TextField(usernamePlaceholder, text: $username)
                         .disableAutocorrection(true)
+                        .autocapitalization(.none)
+                        .font(Font.custom("CircularStd-Book", size: 16))
+                        .foregroundColor(keyboardFocused ? .black : (errorMessage.isEmpty ? .black : .red))
+                        .onTapGesture {
+                            if !errorMessage.isEmpty {
+                                errorMessage = ""
+                            }
+                        }
+                }
+                .padding(.top, -180)
+                .padding(.leading, 20)
+                
+                Rectangle()
+                    .frame(height: 1.0, alignment: .bottom)
+                    .foregroundColor(errorMessage.isEmpty ? Color.gray : Color.red)
+                    .baselineOffset(10)
+                    .focused($keyboardFocused)
+                    .font(.system(size: 16))
+                    .padding(.trailing, 20)
+                    .padding(.leading, 20)
+                    .padding(.top, -155)
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                            keyboardFocused = true
+                        }
+                    }
+                VStack{
+                    Text("Email Address")
+                        .font(Font.custom("CircularStd-Book", size: 14))
+                        .foregroundColor(Color(red: 0.39, green: 0.45, blue: 0.58))
+                        .padding(.top, 20)
+                        .padding(.trailing, 250)
+                        .padding(.top, -150)
+                    HStack{
+                        Image("mail")
+                        TextField(emailPlaceholder, text: $email)
+                            .autocapitalization(.none)
+                            .disableAutocorrection(true)
+                            .font(Font.custom("CircularStd-Book", size: 16))
+                            .foregroundColor(keyboardFocused ? .black : (errorMessage.isEmpty ? .black : .red))
+                            .onTapGesture {
+                                if !errorMessage.isEmpty {
+                                    errorMessage = ""
+                                }
+                            }
+                        
+                    }
+                    .padding(.top, -110)
+                    .padding(.leading, 20)
+                    Rectangle()
+                        .frame(height: 1.0, alignment: .bottom)
+                        .foregroundColor(errorMessage.isEmpty ? Color.gray : Color.red)
+                        .baselineOffset(10)
+                        .padding(.top, -80)
+                        .padding(.trailing, 20)
+                        .padding(.leading, 20)
+                    
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                keyboardFocused = true
+                            }
+                            
+                        }
+                }
+                
+                Text("Password")
+                    .disableAutocorrection(true)
+                    .font(Font.custom("CircularStd-Book", size: 14))
+                    .foregroundColor(Color(red: 0.39, green: 0.45, blue: 0.58))
+                    .padding(.top, -60)
+                    .padding(.trailing, 280)
+                HStack{
+                    Image("lock")
+                    TextField(passwordPlaceholder, text: $password)
+                        .autocapitalization(.none)
                         .font(Font.custom("CircularStd-Book", size: 16))
                         .foregroundColor(keyboardFocused ? .black : (errorMessage.isEmpty ? .black : .red))
                         .onTapGesture {
@@ -154,119 +192,83 @@ struct SignUpView: View {
                         }
                     
                 }
-                .padding(.top, -110)
+                .padding(.top, -40)
                 .padding(.leading, 20)
                 Rectangle()
                     .frame(height: 1.0, alignment: .bottom)
                     .foregroundColor(errorMessage.isEmpty ? Color.gray : Color.red)
                     .baselineOffset(10)
-                    .padding(.top, -80)
                     .padding(.trailing, 20)
                     .padding(.leading, 20)
+                    .padding(.top, -10)
                 
                     .onAppear {
                         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                             keyboardFocused = true
                         }
-                        
                     }
-            }
-            
-            Text("Password")
-                .disableAutocorrection(true)
-                .font(Font.custom("CircularStd-Book", size: 14))
-                .foregroundColor(Color(red: 0.39, green: 0.45, blue: 0.58))
-                .padding(.top, -60)
-                .padding(.trailing, 280)
-            HStack{
-                Image("lock")
-                TextField(passwordPlaceholder, text: $password)
-                    .autocapitalization(.none)
-                    .font(Font.custom("CircularStd-Book", size: 16))
-                    .foregroundColor(keyboardFocused ? .black : (errorMessage.isEmpty ? .black : .red))
-                    .onTapGesture {
-                        if !errorMessage.isEmpty {
-                            errorMessage = ""
-                        }
-                    }
-                
-            }
-            .padding(.top, -40)
-            .padding(.leading, 20)
-            Rectangle()
-                .frame(height: 1.0, alignment: .bottom)
-                .foregroundColor(errorMessage.isEmpty ? Color.gray : Color.red)
-                .baselineOffset(10)
-                .padding(.trailing, 20)
-                .padding(.leading, 20)
-                .padding(.top, -10)
-            
-                .onAppear {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                        keyboardFocused = true
-                    }
-                }
-            
-            VStack{
-                Button(action: {
-                    Task {
-                        if username.isEmpty || email.isEmpty || password.isEmpty {
-                            errorMessage = "Please fill in all the fields."
-                        } else if await isUsernameAvailable() {
-                            await createUser()
-                            showVerificationView = true // Set to true after user creation
-                        } else {
-                            errorMessage = "Username or Email is already taken."
-                        }
-                    }
-                }) {
-                    Text("Sign Up")
-                        .font(Font.custom("CircularStd-Book", size: 16))
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(.white)
-                        .frame(width: 309, height: 56)
-                        .background(Color(red: 0, green: 0.34, blue: 1))
-                        .cornerRadius(10)
-                        .shadow(color: Color(red: 0, green: 0.34, blue: 1).opacity(0.35), radius: 20, x: 0, y: 20)
-                    
-                }
-                
-                
-                NavigationLink(
-                    destination: VerificationView(),
-                    isActive: $showVerificationView,
-                    label: {
-                        EmptyView()
-                    }
-                )
-                
                 
                 VStack{
-                    Text("Log In")
-                        .foregroundColor(.blue)
-                        .font(Font.custom("CircularStd-Book", size: 16))
+                    Button(action: {
+                        Task {
+                            if username.isEmpty || email.isEmpty || password.isEmpty {
+                                errorMessage = "Please fill in all the fields."
+                            } else if await isUsernameAvailable() {
+                                await createUser()
+                                showVerificationView = true // Set to true after user creation
+                            } else {
+                                errorMessage = "Username or Email is already taken."
+                            }
+                        }
+                    }) {
+                        Text("Sign Up")
+                            .font(Font.custom("CircularStd-Book", size: 16))
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.white)
+                            .frame(width: 309, height: 56)
+                            .background(Color(red: 0, green: 0.34, blue: 1))
+                            .cornerRadius(10)
+                            .shadow(color: Color(red: 0, green: 0.34, blue: 1).opacity(0.35), radius: 20, x: 0, y: 20)
+                        
+                    }
+                    
+                    
+                    NavigationLink(
+                        destination: VerificationView(),
+                        isActive: $showVerificationView,
+                        label: {
+                            EmptyView()
+                        }
+                    )
+                    
+                    
+                    VStack{
+                        Text("Log In")
+                            .foregroundColor(.blue)
+                            .font(Font.custom("CircularStd-Book", size: 16))
+                    }
+                    
+                    .padding(.top, 40)
+                    
+                    Spacer()
+                        .navigationBarHidden(true)
+                        .navigationBarBackButtonHidden(true)
                 }
                 
                 .padding(.top, 40)
                 
-                Spacer()
-                    .navigationBarHidden(true)
-                    .navigationBarBackButtonHidden(true)
+                
+                .navigationBarHidden(true)
+                .navigationBarBackButtonHidden(true)
+                
+                .edgesIgnoringSafeArea(.all)
             }
             
-            .padding(.top, 40)
+            .task {
+                await createUser()
+            }
             
-            
-            .navigationBarHidden(true)
-            .navigationBarBackButtonHidden(true)
-            
-            .edgesIgnoringSafeArea(.all)
         }
-        
-        .task {
-            await createUser()
-        }
-        
     }
 }
 
