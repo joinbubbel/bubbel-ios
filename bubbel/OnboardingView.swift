9eb4d1e6766e84970c66b2b4bc8bf3438e2e0b79
Ivoine <ivoinestrachan242@gmail.com>
2023-08-07 13:33:54 -0400

    working on onboarding go to after signup

--- bubbel/OnboardingView.swift
+++ bubbel/OnboardingView.swift
@@ -7,26 +7,71 @@
 
 import SwiftUI
 
+struct ArrowView: View {
+    var body: some View {
+        Image("arrowon")
+    }
+}
+
+struct PageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+    
+    func makeUIView(context: Context) -> UIPageControl {
+        let pageControl = UIPageControl()
+        pageControl.numberOfPages = numberOfPages
+        pageControl.addTarget(context.coordinator, action: #selector(Coordinator.updateCurrentPage(sender:)), for: .valueChanged)
+        return pageControl
+    }
+    
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject {
+        var control: PageControl
+        
+        init(_ control: PageControl) {
+            self.control = control
+        }
+        
+        @objc func updateCurrentPage(sender: UIPageControl) {
+            control.currentPage = sender.currentPage
+        }
+    }
+}
 struct OnboardingView: View {
+    @State private var currentPageIndex = 0
+    
     init() {
         UIPageControl.appearance().currentPageIndicatorTintColor = .blue
         UIPageControl.appearance().pageIndicatorTintColor = UIColor.black.withAlphaComponent(0.2)
     }
     var body: some View {
         VStack{
-            TabView {
+            TabView(selection: $currentPageIndex) {
                 Onboard1()
-                    .tag(1)
+                    .tag(0)
                 Onboard2()
-                    .tag(2)
+                    .tag(1)
                 Onboard3()
-                    .tag(3)
+                    .tag(2)
                 Onboard4()
-                    .tag(4)
+                    .tag(3)
             }
             .tabViewStyle(PageTabViewStyle())
-            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
-            
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .never))
+            HStack {
+                
+                if currentPageIndex == 3 {
+                    ArrowView()
+                }
+                
+            }
             
             Spacer()
                 .navigationBarHidden(true)
