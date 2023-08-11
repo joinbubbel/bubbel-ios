4f22f54d9b505d2c70669676cec88efce9e77672
Ivoine <ivoinestrachan242@gmail.com>
2023-08-10 22:43:39 -0400

    setting up create club n club profile

---
+++ bubbel/Screens/Club/ClubProfile.swift
@@ -0,0 +1,20 @@
+//
+//  ClubProfile.swift
+//  bubbel
+//
+//  Created by Ivoine Strachan on 10/08/2023.
+//
+
+import SwiftUI
+
+struct ClubProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ClubProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        ClubProfile()
+    }
+}
---
+++ bubbel/Screens/Club/CreateClub.swift
@@ -0,0 +1,20 @@
+//
+//  CreateClub.swift
+//  bubbel
+//
+//  Created by Ivoine Strachan on 10/08/2023.
+//
+
+import SwiftUI
+
+struct CreateClub: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateClub_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateClub()
+    }
+}
